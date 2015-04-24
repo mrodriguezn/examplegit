05c8e926eed1de1d162afa91561fb48be548080f
mrodriguezn <mscoo33@gmail.com>
2015-04-23 22:59:19 -0600

    Give a personalized hello.

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,13 @@
-
+import java.util.Scanner;
 public class Main {
 
+	
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+		Scanner in = new Scanner(System.in);
+		String name;
+		System.out.print("Enter your name: ");
+		name = in.nextLine();
+		System.out.println("Hello "+name+"!");
 	}
 
 }
