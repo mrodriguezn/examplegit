5b4682a7357c33bde29735337ec4517cee447704
mrodriguezn <mscoo33@gmail.com>
2015-04-23 23:06:19 -0600

    Prompt user for last name as well.
    (Note: I am commiting as a different user here to show something later on)
    
    Signed-off-by: Alfonso <mscoo33@hotmail.com>

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,13 @@ public class Main {
 	
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		String name;
-		System.out.print("Enter your name: ");
+		String name, lastname;
+		System.out.print("Enter your first name: ");
 		name = in.nextLine();
-		System.out.println("Hello "+name+"!");
+		
+		System.out.print("Now enter your last name: ");
+		lastname = in.nextLine();
+		System.out.println("Hello "+name+" "+ lastname+"!");
 	}
 
 }
